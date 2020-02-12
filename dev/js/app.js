b9b3d480ec507146d5c382b01740484b5eccd892
Adam Hemzal <adahemzal@gmail.com>
2020-02-12 14:27:15 +0100

    huge update

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
     //SOME JQUERY CODE HERE
-
+    $('.slider').slick({
+        slidesToShow: 1,
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 8000,
+      });
 });
