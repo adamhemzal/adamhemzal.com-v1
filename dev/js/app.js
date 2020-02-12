9ac452aa56f6cbf6b91c94fe83fa76df1b142528
Adam Hemzal <adahemzal@gmail.com>
2020-02-12 19:06:48 +0100

    almost done

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
 
-    //SOME JQUERY CODE HERE
-    $('.slider').slick({
-        slidesToShow: 1,
-        dots: true,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 8000,
-      });
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+      $("header").addClass("box-shadow-scroll");
+    } else {
+      $("header").removeClass("box-shadow-scroll");
+    }
+  }); 
+
+  $(".header-photo")
+  .mouseover(function() {
+    $(this).attr("srcset", "img/me/adam-hemzal-real-optimized.png, img/me/adam-hemzal-real.png 2x");
+  })
+  .mouseout(function() {
+    $(this).attr("srcset", "img/me/adam-hemzal-drawing-optimized.png, img/me/adam-hemzal-drawing.png 2x");
+  });
+    
+  $('.slider').slick({
+      slidesToShow: 1,
+      dots: true,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 8000,
+    });
+  
 });
